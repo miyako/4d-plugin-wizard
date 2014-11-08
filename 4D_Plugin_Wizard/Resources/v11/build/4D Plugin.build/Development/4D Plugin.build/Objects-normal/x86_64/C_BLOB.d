2e0820a28272821c1df6ab36b27514237ca791e7
miyako <keisuke.miyako@4d.com>
2014-11-09 04:50:41 +0900

    composite v11/v14 SDK

---
+++ 4D_Plugin_Wizard/Resources/v11/build/4D Plugin.build/Development/4D Plugin.build/Objects-normal/x86_64/C_BLOB.d
@@ -0,0 +1,19 @@
+dependencies: \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/4D\ Plugin\ API/Classes/C_BLOB.cpp \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/4D\ Plugin\ API/Classes/C_BLOB.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/v11/4D\ Plugin\ API/4DPluginAPI.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/v11/4D\ Plugin\ API/Flags.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/v11/4D\ Plugin\ API/PublicTypes.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/v11/4D\ Plugin\ API/../../4D\ Plugin\ API/Classes/C_TEXT.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/v11/4D\ Plugin\ API/../../4D\ Plugin\ API/Classes/C_INTEGER.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/v11/4D\ Plugin\ API/../../4D\ Plugin\ API/Classes/C_LONGINT.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/v11/4D\ Plugin\ API/../../4D\ Plugin\ API/Classes/C_TIME.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/v11/4D\ Plugin\ API/../../4D\ Plugin\ API/Classes/C_DATE.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/v11/4D\ Plugin\ API/../../4D\ Plugin\ API/Classes/C_REAL.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/v11/4D\ Plugin\ API/../../4D\ Plugin\ API/Classes/C_PICTURE.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/v11/4D\ Plugin\ API/../../4D\ Plugin\ API/Classes/C_POINTER.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/4D\ Plugin\ API/Classes/ARRAY_TEXT.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/4D\ Plugin\ API/Classes/ARRAY_INTEGER.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/4D\ Plugin\ API/Classes/ARRAY_LONGINT.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/4D\ Plugin\ API/Classes/ARRAY_REAL.h \
+  /Users/miyako/github-miyako/4d-plugin-14-wizard-miyako-version/4D_Plugin_Wizard/Resources/4D\ Plugin\ API/Classes/ARRAY_BOOLEAN.h
